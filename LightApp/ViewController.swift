7642e0383fce44fb73588771471a5d47a9297e3a
JohnKostanov <kostanovd@icloud.com>
2019-07-04 23:57:40 +0200

    Initial Commit

---
+++ LightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LightApp
+//
+//  Created by  Джон Костанов on 04/07/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
